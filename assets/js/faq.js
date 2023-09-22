36ea1b56f7e9c176d6c783661a087d75f25d85e6
zihui <101386735+N0nEscapeSh3d0w@users.noreply.github.com>
2023-09-23 05:35:41 +0800

    Add files via upload

---
+++ assets/js/faq.js
@@ -0,0 +1,15 @@
+// Select all toggle buttons
+const toggleButtons = document.querySelectorAll('.toggle-button');
+
+// Add click event listeners to toggle buttons
+toggleButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Toggle the visibility of the answer section
+        const answer = button.parentElement.nextElementSibling;
+        answer.classList.toggle('hidden');
+        
+        // Change the button text from "+" to "-" or vice versa
+        button.textContent = button.textContent === '+' ? '-' : '+';
+    });
+});
+
